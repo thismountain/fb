4eafecee7be742922defc6449a08e0469a99d77b
thismountain <dltksdhkd9@gmail.com>
2024-07-23 21:11:18 +0900

    hw

---
+++ fb3-1.h
@@ -0,0 +1,24 @@
+extern int yylineno; /* from lexer */
+void yyerror(char *s, ...);
+
+/* nodes in the Abstract Syntax Tree */
+struct ast {
+  int nodetype;
+  struct ast *l;
+  struct ast *r;
+};
+
+struct numval {
+  int nodetype;			/* type K */
+  double number;
+};
+
+/* build an AST */
+struct ast *newast(int nodetype, struct ast *l, struct ast *r);
+struct ast *newnum(double d);
+
+/* evaluate an AST */
+double eval(struct ast *);
+
+/* delete and free an AST */
+void treefree(struct ast *);
